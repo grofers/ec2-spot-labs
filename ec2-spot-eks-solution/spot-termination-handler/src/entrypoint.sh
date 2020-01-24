b8018dfe7a239bf5ad3b6cc7da73e1e3a0812516
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2020-01-24 17:34:04 +0530

    delete user local data to force pod eviction in case pods use emptyDir

--- ec2-spot-eks-solution/spot-termination-handler/src/entrypoint.sh
+++ ec2-spot-eks-solution/spot-termination-handler/src/entrypoint.sh
@@ -59,7 +59,7 @@ MESSAGE="Spot Termination${CLUSTER_INFO}: ${NODE_NAME}, Instance: ${INSTANCE_ID}
 
 # Drain the node.
 # https://kubernetes.io/docs/tasks/administer-cluster/safely-drain-node/#use-kubectl-drain-to-remove-a-node-from-service
-kubectl drain ${NODE_NAME} --force --ignore-daemonsets
+kubectl drain ${NODE_NAME} --force --ignore-daemonsets --delete-local-data
 
 # Sleep for 200 seconds to prevent this script from looping.
 # The instance should be terminated by the end of the sleep.
